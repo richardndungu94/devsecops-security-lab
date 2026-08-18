4fb1187ac7c4e02c36a506cd22f141d66e9351ce
richardndungu94 <richardndungu052@gmail.com>
2026-08-18 12:11:53 -0400

    secretes semgrep

--- app/server.js
+++ app/server.js
@@ -7,6 +7,16 @@ app.get("/", (req, res) => {
   res.send("DevSecOps Security Lab");
 });
 
+app.get("/user", (req, res) => {
+  const user = req.query.name;
+
+  // Intentionally vulnerable code for the security lab
+  const query = `SELECT * FROM users WHERE name = '${user}'`;
+
+  res.json({
+    query
+  });
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
